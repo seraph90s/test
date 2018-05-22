e447fa2ff374df3aa0425c7cd27a025719b9478c
seraph90s <39481686+seraph90s@users.noreply.github.com>
2018-05-22 12:29:49 +0800

    Update test-1.py

--- test-1.py
+++ test-1.py
@@ -10,15 +10,22 @@ print('hello world!') #输出hello world
 1!=2<3
 int(10.02323)
 float(20)
+
 import math
 pow(2,4),2**4
 abs(4-16),sum((1,2,3,4))
 min(3,1,2,4),max(3,1,2,4)
+
+# 导入随机数模块
 import random
-one=random.random()#为'one'赋值=生成一个0到1的随机浮点数
+# 为'one'赋值=生成一个0到1的随机浮点数
+one=random.random()
 print(one)
-two=random.choice(['qytang','ccie','pass','network'])#为‘two’赋值=在qytang、ccie、pass、network中随机选取一个数
+
+#为‘two’赋值=在qytang、ccie、pass、network中随机选取一个数
+two=random.choice(['qytang','ccie','pass','network'])
 print(two)
+
 # 随机产生IP地址四个段落的数字
 section1=random.randint(1,255)
 section2=random.randint(1,255)
