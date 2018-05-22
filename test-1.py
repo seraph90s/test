a141dc237f72d06c6d0340724056b20235025316
seraph90s <869634087@qq.com>
2018-05-22 12:17:31 +0800

    commit 2

--- test-1.py
+++ test-1.py
@@ -1,3 +1,29 @@
-print('hello world!')
+print('hello world!') #输出hello world
+10/4
+-10/4
+10/4.0
+-10/4.0
+10//4
+10%4
+0o1,0o20,0o377
+1==2<3
+1!=2<3
+int(10.02323)
+float(20)
+import math
+pow(2,4),2**4
+abs(4-16),sum((1,2,3,4))
+min(3,1,2,4),max(3,1,2,4)
 import random
-random.randint (1,20)
+one=random.random()#为'one'赋值=生成一个0到1的随机浮点数
+print(one)
+two=random.choice(['qytang','ccie','pass','network'])#为‘two’赋值=在qytang、ccie、pass、network中随机选取一个数
+print(two)
+# 随机产生IP地址四个段落的数字
+section1=random.randint(1,255)
+section2=random.randint(1,255)
+section3=random.randint(1,255)
+section4=random.randint(1,255)
+random_ip=str(section1) +'.' +str(section2)+'.'+str(section3)+'.'+str(section4)
+
+print(random_ip)
